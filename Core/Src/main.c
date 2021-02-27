48595377ba8e8661c351de5da5c9724ec61f36e1
Ivan <im.ivan.im@gmail.com>
2021-02-28 00:53:17 +0100

    Adding pwm functionality

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -42,6 +42,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc3;
 
 CRC_HandleTypeDef hcrc;
 
@@ -90,6 +91,7 @@ static void MX_FMC_Init(void);
 static void MX_LTDC_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_TIM3_Init(void);
+static void MX_ADC3_Init(void);
 void TouchGFX_Task(void *argument);
 void StartBlinkTask(void *argument);
 void StartPwmTask(void *argument);
@@ -148,6 +150,7 @@ int main(void)
   MX_LTDC_Init();
   MX_USART1_UART_Init();
   MX_TIM3_Init();
+  MX_ADC3_Init();
   MX_TouchGFX_Init();
   /* USER CODE BEGIN 2 */
 
@@ -268,6 +271,56 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC3_Init(void)
+{
+
+  /* USER CODE BEGIN ADC3_Init 0 */
+
+  /* USER CODE END ADC3_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC3_Init 1 */
+
+  /* USER CODE END ADC3_Init 1 */
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc3.Instance = ADC3;
+  hadc3.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV8;
+  hadc3.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc3.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc3.Init.ContinuousConvMode = DISABLE;
+  hadc3.Init.DiscontinuousConvMode = DISABLE;
+  hadc3.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc3.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc3.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc3.Init.NbrOfConversion = 1;
+  hadc3.Init.DMAContinuousRequests = DISABLE;
+  hadc3.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC3_Init 2 */
+
+  /* USER CODE END ADC3_Init 2 */
+
 }
 
 /**
@@ -581,6 +634,18 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+uint32_t GetPotVal(void)
+{
+	if (HAL_ADC_Start(&hadc3) == HAL_OK)
+	{
+		if (HAL_ADC_PollForConversion(&hadc3, 1000) == HAL_OK)
+		{
+			return HAL_ADC_GetValue(&hadc3);
+		}
+	}
+	return  0xFFFFFFFF;
+}
+
 void setPWM(TIM_HandleTypeDef timer, uint32_t channel, uint16_t period, uint16_t pulse)
 {
 	HAL_TIM_PWM_Stop(&timer, channel); // stop generation of pwm
@@ -648,11 +713,8 @@ void StartPwmTask(void *argument)
   /* Infinite loop */
   for(;;)
   {
-	  for(int i=0; i<256; i++)
-	  {
-		  setPWM(htim3, TIM_CHANNEL_1, 255, i);
-		  osDelay(10);
-	  }
+	  setPWM(htim3, TIM_CHANNEL_1, 4095, GetPotVal());
+	  osDelay(1);
   }
   /* USER CODE END StartPwmTask */
 }
